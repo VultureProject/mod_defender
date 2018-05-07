0324a9207f994ed3b2c01b88da377da4b87fb0ab
Kevin GUILLEMOT <kguillemot@p0144>
2018-05-07 13:54:25 +0200

    Travis : Remove fixed certificates and generates them dynamically (used by https Apache tests)

--- tests/travis/https.sh
+++ tests/travis/https.sh
@@ -3,8 +3,14 @@
 set -e
 
 sudo mkdir /etc/apache2/ssl
-sudo cp tests/cert/apache.crt /etc/apache2/ssl
-sudo cp tests/cert/apache.key /etc/apache2/ssl
+cd /etc/apache2/ssl
+# Create the PKI used by Apache (https tests)
+sudo openssl genrsa -out ca.key 4096
+echo -e "FR\nNord\nLille\nVultureProject\nTravis tests\nAC_racine\nsupport@vultureproject.org\n\n" | sudo openssl req -sha256 -new -x509 -key ./ca.key -out ./ca.crt
+sudo openssl genrsa -out localhost.key 4096
+echo -e "FR\nNord\nLille\nVultureProject\nTravis tests\nlocalhost\nsupport@vultureproject.org\n\n" | sudo openssl req -sha256 -new -key ./localhost.key -out ./localhost.csr
+sudo openssl x509 -req -sha256 -days 1 -in ./localhost.csr -CA ./ca.crt -CAkey ./ca.key -CAcreateserial -out ./localhost.crt
+
 
 sudo mkdir /etc/defender/
 sudo wget -O /etc/defender/core.rules https://raw.githubusercontent.com/nbs-system/naxsi/master/naxsi_config/naxsi_core.rules
@@ -25,12 +31,14 @@ sudo service apache2 stop
 printf \
     "<IfModule mod_ssl.c>
     <VirtualHost localhost:443>
+        ServerName localhost
         LogLevel notice
         AllowEncodedSlashes On
         ErrorLog \${APACHE_LOG_DIR}/error.log
         SSLEngine on
-        SSLCertificateFile /etc/apache2/ssl/apache.crt
-        SSLCertificateKeyFile /etc/apache2/ssl/apache.key
+        SSLCertificateFile /etc/apache2/ssl/localhost.crt
+        SSLCertificateKeyFile /etc/apache2/ssl/localhost.key
+        SSLCACertificateFile /etc/apache2/ssl/ca.crt
         <Location />
             <IfModule defender_module>
                 Defender On
@@ -52,6 +60,8 @@ printf \
     </VirtualHost>
 </IfModule>" | sudo tee /etc/apache2/sites-available/ssl-default.conf
 
+sudo a2ensite ssl-default
+cd $DEFENDER_HOME
 cmake -H. -Bbuild
 cmake --build build
 sudo cp build/mod_defender.so /usr/lib/apache2/modules/
